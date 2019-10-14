891912c5645394e0aa6ef2f87450b681a32ee15a
counterthreatunit <counterthreatunit@users.noreply.github.com>
2019-10-14 15:56:04 -0400

    setup for building wheel

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aristotle-dwharton",
+    version="1.0.0",
+    author="David Wharton",
+    description="A a Python script and library for the viewing and filtering of Suricata and Snort rulesets based on interpreted key-value pairs present in the metadata keyword within each rule.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/secureworks/aristotle,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Networking :: Monitoring,
+        "Topic :: System :: Networking :: Firewalls",
+    ],
+    python_requires='>=2.7',
+    keywords='suricata, snort, metadata, ruleset, BETTER, IDS, IPS, signatures',
+    project_urls={
+        'Documentation': 'https://aristotle-better.readthedocs.io/',
+        'Source': 'https://github.com/secureworks/aristotle,
+    },
+)
+
